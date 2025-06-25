8fa2292d99e370649859e9be4769fe69b81e62e7
Danillo Ferreira <danillodana@outlook.com>
2025-06-25 17:20:56 -0300

    fix(webscrapping.ts): httpsAgent ignore Unauthorized

--- src/utils/web-scrapping.ts
+++ src/utils/web-scrapping.ts
@@ -1,107 +1,119 @@
 import * as cheerio from "cheerio"
 import axios from "axios"
+import https from "https"
+
+const insecureAgent = new https.Agent({ rejectUnauthorized: false })
 
 const getFeaturedMECNews = async () => {
-  try {
-    // Esta url esta pegando a ultima noticia em destaque
-    const { data } = await axios.get("https://www.gov.br/mec/pt-br")
+	try {
+		// Esta url esta pegando a ultima noticia em destaque
+		const { data } = await axios.get("https://www.gov.br/mec/pt-br", {
+			httpsAgent: insecureAgent
+		})
 
-    const $ = cheerio.load(data)
+		const $ = cheerio.load(data)
 
-    const newsLink = $(".foto-sobreposta-grande a.imag").first().attr("href")
+		const newsLink = $(".foto-sobreposta-grande a.imag").first().attr("href")
 
-    if (!newsLink) {
-      return {
-        Error: {
-          status: 400,
-          msg: "Não foi possível selecionar o link da notícia"
-        }
-      }
-    }
-    // Abre a noticia mais recente
-    const { data: newsHtml } = await axios.get(newsLink!)
-    const $news = cheerio.load(newsHtml)
+		if (!newsLink) {
+			return {
+				Error: {
+					status: 400,
+					msg: "Não foi possível selecionar o link da notícia"
+				}
+			}
+		}
+		// Abre a noticia mais recente
+		const { data: newsHtml } = await axios.get(newsLink!, {
+			httpsAgent: insecureAgent
+		})
+		const $news = cheerio.load(newsHtml)
 
-    const title = $news("h1").text().trim()
-    const subtitle = $news("div.documentDescription").text().trim()
-    const PublishedDate = $news("span.documentPublished .value").text().trim()
+		const title = $news("h1").text().trim()
+		const subtitle = $news("div.documentDescription").text().trim()
+		const PublishedDate = $news("span.documentPublished .value").text().trim()
 
-    // Pega o conteudo html da parte da noticia para filtrar apenas o texto
-    const htmlContent = $news("#parent-fieldname-text").html()
+		// Pega o conteudo html da parte da noticia para filtrar apenas o texto
+		const htmlContent = $news("#parent-fieldname-text").html()
 
-    if (!htmlContent) {
-      throw new Error("News content not found.")
-    }
+		if (!htmlContent) {
+			throw new Error("News content not found.")
+		}
 
-    const $content = cheerio.load(htmlContent)
+		const $content = cheerio.load(htmlContent)
 
-    // Extrai e limpa o texto
-    const newsContent = $content.text().trim()
+		// Extrai e limpa o texto
+		const newsContent = $content.text().trim()
 
-    return {
-      titulo: title,
-      subtitulo: subtitle,
-      corpo: newsContent,
-      data: PublishedDate,
-      url: newsLink
-    }
-  } catch (error) {
-    console.error("Error accessing MEC website:", error)
-    return { error: "Erro ao acessar o site do MEC" }
-  }
+		return {
+			titulo: title,
+			subtitulo: subtitle,
+			corpo: newsContent,
+			data: PublishedDate,
+			url: newsLink
+		}
+	} catch (error) {
+		console.error("Error accessing MEC website:", error)
+		return { error: "Erro ao acessar o site do MEC" }
+	}
 }
 
 const getLatestMECNews = async () => {
-  try {
-    // Esta url pega a noticia mais recente
-    const { data } = await axios.get(
-      "https://www.gov.br/mec/pt-br/assuntos/noticias"
-    )
+	try {
+		// Esta url pega a noticia mais recente
+		const { data } = await axios.get(
+			"https://www.gov.br/mec/pt-br/assuntos/noticias",
+			{
+				httpsAgent: insecureAgent
+			}
+		)
 
-    const $ = cheerio.load(data)
+		const $ = cheerio.load(data)
 
-    // alterar para pegar a primeira de todas
-    const newsLink = $(".noticias li .titulo a").first().attr("href")
+		// alterar para pegar a primeira de todas
+		const newsLink = $(".noticias li .titulo a").first().attr("href")
 
-    if (!newsLink) {
-      return {
-        Error: {
-          status: 400,
-          msg: "Não foi possível selecionar o link da notícia"
-        }
-      }
-    }
-    // Abre a noticia mais recente
-    const { data: newsHtml } = await axios.get(newsLink!)
-    const $news = cheerio.load(newsHtml)
+		if (!newsLink) {
+			return {
+				Error: {
+					status: 400,
+					msg: "Não foi possível selecionar o link da notícia"
+				}
+			}
+		}
+		// Abre a noticia mais recente
+		const { data: newsHtml } = await axios.get(newsLink!, {
+			httpsAgent: insecureAgent
+		})
+		const $news = cheerio.load(newsHtml)
 
-    const title = $news("h1").text().trim()
-    const subtitle = $news("div.documentDescription").text().trim()
-    const PublishedDate = $news("span.documentPublished .value").text().trim()
+		const title = $news("h1").text().trim()
+		const subtitle = $news("div.documentDescription").text().trim()
+		const PublishedDate = $news("span.documentPublished .value").text().trim()
 
-    // Pega o conteudo html da parte da noticia para filtrar apenas o texto
-    const contentHtml = $news("#parent-fieldname-text").html()
+		// Pega o conteudo html da parte da noticia para filtrar apenas o texto
+		const contentHtml = $news("#parent-fieldname-text").html()
 
-    if (!contentHtml) {
-      throw new Error("Conteúdo da notícia não encontrado")
-    }
+		if (!contentHtml) {
+			throw new Error("Conteúdo da notícia não encontrado")
+		}
 
-    const $content = cheerio.load(contentHtml)
+		const $content = cheerio.load(contentHtml)
 
-    // Extrai e limpa o texto
-    const newsContent = $content.text().trim()
+		// Extrai e limpa o texto
+		const newsContent = $content.text().trim()
 
-    return {
-      titulo: title,
-      subtitulo: subtitle,
-      corpo: newsContent,
-      data: PublishedDate,
-      url: newsLink
-    }
-  } catch (error) {
-    console.error("Erro ao acessar o site do MEC:", error)
-    return { error: "Erro ao acessar o site do MEC" }
-  }
+		return {
+			titulo: title,
+			subtitulo: subtitle,
+			corpo: newsContent,
+			data: PublishedDate,
+			url: newsLink
+		}
+	} catch (error) {
+		console.error("Erro ao acessar o site do MEC:", error)
+		return { error: "Erro ao acessar o site do MEC" }
+	}
 }
 
 export const webScrapping = { getFeaturedMECNews, getLatestMECNews }
