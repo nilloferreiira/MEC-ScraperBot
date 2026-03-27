85bb767fa88a9fd7dcb33e015911d83747bbba93
Danillo Ferreira <77707956+nilloferreiira@users.noreply.github.com>
2026-03-27 13:58:55 -0300

    Enhance axios request with custom headers
    Added custom headers to the axios request for web scraping.

--- src/utils/web-scrapping.ts
+++ src/utils/web-scrapping.ts
@@ -71,7 +71,14 @@ const getLatestMECNews = async () => {
 		const { data } = await axios.get(
 			"https://www.gov.br/mec/pt-br/assuntos/noticias",
 			{
-				httpsAgent: insecureAgent
+				httpsAgent: insecureAgent,
+				headers: {
+	                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+	                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,all;q=0.8',
+	                'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+	                'Referer': 'https://www.google.com/',
+	                'Cache-Control': 'no-cache'
+	            }
 			}
 		)
 
